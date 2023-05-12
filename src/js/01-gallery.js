3971f879bd79d84a5179191a5375002591af1bc6
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-12 00:37:43 -0500

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const action = (imageAction) => {
+    imageAction.preventDefault();
+}
+
+const  createGallery = (items) =>{
+    return items.map((item) =>
+     `<li class='gallery__item'>
+        <a class='gallery__link' href='${item.original}'>
+            <img
+                class='gallery__image'
+                src= '${item.preview}'
+                alt= '${item.description}'
+            />
+        </a>  
+    </li>`).join('');
+}
+
+const addGallery = createGallery(galleryItems);
+gallery.innerHTML = addGallery;
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+console.log(lightbox);
