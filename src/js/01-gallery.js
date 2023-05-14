c42155c56ff4f44de33bff678addd56c07b34087
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-14 12:00:45 -0500

    configuracion prettier y .json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,13 +4,15 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-const action = (imageAction) => {
-    imageAction.preventDefault();
-}
+const action = imageAction => {
+	imageAction.preventDefault();
+};
 
-const  createGallery = (items) =>{
-    return items.map((item) =>
-     `<li class='gallery__item'>
+const createGallery = items => {
+	return items
+		.map(
+			item =>
+				`<li class='gallery__item'>
         <a class='gallery__link' href='${item.original}'>
             <img
                 class='gallery__image'
@@ -18,11 +20,16 @@ const  createGallery = (items) =>{
                 alt= '${item.description}'
             />
         </a>  
-    </li>`).join('');
-}
+    </li>`
+		)
+		.join('');
+};
 
 const addGallery = createGallery(galleryItems);
 gallery.innerHTML = addGallery;
 
-let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+let lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
 console.log(lightbox);
