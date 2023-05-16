33783c49f0876c5b1ac080483f6e7e739c7c01b9
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-16 13:20:36 -0500

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,26 @@ import Vimeo from '@vimeo/player'; // importacion de la biblioteca de vimeo
 import _ from 'lodash'; // importatcion de la funcion throttle de lodash
 
 const player = new Vimeo(document.querySelector('#vimeo-player')); // Hacemos referencia al id del iframe
-
+/*
 const Async = async () => {
+	// funcion asincromatica
 	try {
 		const seconds = await player.getCurrentTime();
 		localStorage.setItem('videoplayer-current-time', seconds); //Guardamos el tiempo dentro del almacenamiento local
+	} catch (error) {
+		console.error(
+			'Error al obtener el tiempo actual de reproducción del video',
+			error
+		);
+	}
+};
+*/
+const Async = () => {
+	// función asíncrona
+	try {
+		player.getCurrentTime().then(seconds => {
+			localStorage.setItem('videoplayer-current-time', seconds); // Guardamos el tiempo dentro del almacenamiento local
+		});
 	} catch (error) {
 		console.error(
 			'Error al obtener el tiempo actual de reproducción del video',
