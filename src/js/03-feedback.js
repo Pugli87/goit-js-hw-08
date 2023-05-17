ceec9727dfeb927e2e40b8482b3629504e539d3e
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-17 12:17:39 -0500

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,17 @@
 import _ from 'lodash'; // importatcion de la funccion throttle de lodash
 
 // Función para guardar el estado del formulario en el almacenamiento local
-function saveFormState() {
+const saveFormState = () => {
 	const formState = {
 		email: document.querySelector('input[name="email"]').value,
 		message: document.querySelector('textarea[name="message"]').value,
 	};
 
 	localStorage.setItem('feedback-form-state', JSON.stringify(formState));
-}
+};
 
 // Función para cargar el estado del formulario desde el almacenamiento local
-function loadFormState() {
+const loadFormState = () => {
 	const savedState = localStorage.getItem('feedback-form-state');
 
 	if (savedState) {
@@ -20,17 +20,17 @@ function loadFormState() {
 		document.querySelector('textarea[name="message"]').value =
 			formState.message;
 	}
-}
+};
 
 // Función para borrar el estado del formulario y los campos del almacenamiento local
-function clearFormState() {
+const clearFormState = () => {
 	localStorage.removeItem('feedback-form-state');
 	document.querySelector('input[name="email"]').value = '';
 	document.querySelector('textarea[name="message"]').value = '';
-}
+};
 
 // Función para manejar el envío del formulario
-function handleSubmit(event) {
+const handleSubmit = event => {
 	event.preventDefault();
 
 	const email = document.querySelector('input[name="email"]').value;
@@ -41,7 +41,7 @@ function handleSubmit(event) {
 	console.log('Message:', message);
 
 	clearFormState();
-}
+};
 
 // Añadir evento input al formulario para guardar el estado del formulario en el almacenamiento local
 const form = document.querySelector('.feedback-form');
